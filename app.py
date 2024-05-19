b321f79f148f4ca177ef13f6d20046cebc92b371
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2024-05-19 11:14:07 -0500

    Refactor app.py to use a factory function for creating the Flask app

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, jsonify
 from service.convert_service import convert_yt_to_text
 
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False  # Configura Flask para que no use ASCII para JSON
+def create_app():
+    app = Flask(__name__)
+    app.config['JSON_AS_ASCII'] = False  # Configura Flask para que no use ASCII para JSON
 
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+    @app.route('/')
+    def home():
+        return 'Hello, World!'
 
-@app.route('/convert', methods=['POST'])
-def convert():
-    return jsonify(convert_yt_to_text(request.json['url'])), 200
+    @app.route('/convert', methods=['POST'])
+    def convert():
+        return jsonify(convert_yt_to_text(request.json['url'])), 
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app()
+    app.run()
